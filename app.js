b5f95ab955e39e5c661ac905332cdf97b3275394
peipeipeipeigit <szupei21@gmail.com>
2023-06-30 02:18:35 +0800

    setting categorySeeder.js and recordSeeder.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // basic setting
 const express = require('express')
+const exphbs = require('express-handlebars') 
 const app = express()
 const port = 3000
 
-// setting the route and corresponding response
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting the route
 app.get('/', (req, res) => {
   res.send('This is 老爸的私房錢yo')
 })
 
-// Listen the server when it started
+// Listen the server
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
