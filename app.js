32e086b7a9e6e80b6782d9288a43f027f20d227e
peipeipeipeigit <szupei21@gmail.com>
2023-06-30 02:59:34 +0800

    setting and connecting mongoose

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars') 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// setting database
+// setting and connecting database
 const mongoose = require('mongoose') 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 mongoose.connect(process.env.MONGODB_URI) 
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
