99ceb0ba69b4165a15d5e46b527d187fa468551a
peipeipeipeigit <szupei21@gmail.com>
2023-06-30 15:01:43 +0800

    create ./config/mongoose.js

--- app.js
+++ app.js
@@ -4,22 +4,11 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
-// setting and connecting database
-const mongoose = require('mongoose') 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI) 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+// require to connect database
+require('./config/mongoose.js')
 
 // setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting the route
